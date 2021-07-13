5f3354b722e6c974f0f447f4a18253cf1b9ae5e9
miht.sem <miht.sem@yandex.com>
2021-07-13 07:10:12 +0500

    Удален лишний код

--- knuth-morirs-pratt.py
+++ knuth-morirs-pratt.py
@@ -1,46 +1,3 @@
-# t = 'лилила'
-#
-# p = [0] * len(t)
-# j = 0
-# i = 1
-# while i < len(t):
-#     if t[j] == t[i]:
-#         p[i] = j + 1
-#         i += 1
-#         j += 1
-#     else:
-#         if j == 0:
-#             p[i] = 0
-#             i += 1
-#         else:
-#             j = p[j - 1]
-#
-# print(p)
-#
-# a = 'лилилось лилилась'
-# m = len(t)
-# n = len(a)
-#
-# i = 0
-# j = 0
-#
-# while i < n:
-#     if a[i] == t[j]:
-#         i += 1
-#         j += 1
-#         if j == m:
-#             print('Nice')
-#             break
-#     else:
-#         if j > 0:
-#             j = p[j - 1]
-#         else:
-#             i += 1
-#
-# if i == n and j != m:
-#     print('Bad')
-
-
 whatToFind = 'лилила'
 masPrefix = [0] * len(whatToFind)
 
@@ -57,7 +14,7 @@ while j < len(whatToFind):
             masPrefix[j] = 0
             j += 1
         else:
-            i = masPrefix[i-1]
+            i = masPrefix[i - 1]
 
 print(masPrefix)
 
